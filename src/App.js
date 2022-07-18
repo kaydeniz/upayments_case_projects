bd5020353a7325daeeea42745f462f36d99921f5
kemalaydeniz <kaydeniz@gmail.com>
2022-07-18 10:28:46 +0300

    Redirect feature added for Products

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import ProductPage from "./Pages/ProductPage"
 import CreateProduct from "./Pages/CreateProduct"
 import Header from "./Components/Header";
 import {Stack} from "@mui/material";
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 
 function App() {
     return (
         <Stack spacing={1} className="Layout-Style">
             <Header/>
             <Routes>
+                <Route
+                    path="*"
+                    element={<Navigate to="/products" replace/>}
+                />
                 <Route path="products" element={<HomePage/>}/>
                 <Route path="products/:productId" element={<ProductPage/>}/>
                 <Route path="createProduct" element={<CreateProduct/>}/>
